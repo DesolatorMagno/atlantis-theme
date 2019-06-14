ad4d33937f2e9ae6544d6671060886270f0e211c
Reny Ramos <renyramosgarcia@gmail.com>
2019-06-14 19:12:34 -0400

    Sidebar esta limpio

--- src/resources/views/layouts/sidebar.blade.php
+++ src/resources/views/layouts/sidebar.blade.php
@@ -1,56 +1,10 @@
 <div class="sidebar sidebar-style-2" data-background-color="{{ config('theme.themes.' . config('theme.active') . '.sidebar') }}">
     <div class="sidebar-wrapper scrollbar scrollbar-inner">
         <div class="sidebar-content">
-            <div class="user">
-                <div class="avatar-sm float-left mr-2">
-                    <img src="{{ asset('vendor/atlantis-theme/img/profile.jpg') }}" alt="..." class="avatar-img rounded-circle">
-                </div>
-                <div class="info">
-                    <a data-toggle="collapse" href="#collapseExample" aria-expanded="true">
-                        <span>
-                            Hizrian
-                            <span class="user-level">Administrator</span>
-                            <span class="caret"></span>
-                        </span>
-                    </a>
-                    <div class="clearfix"></div>
-
-                    <div class="collapse in" id="collapseExample">
-                        <ul class="nav">
-                            <li>
-                                <a href="#profile">
-                                    <span class="link-collapse">My Profile</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#edit">
-                                    <span class="link-collapse">Edit Profile</span>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#settings">
-                                    <span class="link-collapse">Settings</span>
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-            @section('name')
-
-            @endsection
+            @yield('pre-navbar')
             <ul class="nav nav-primary">
-                @section('nav-bar-items')
-
-                @endsection
-                @include('atlantis-theme.examples.partials.dashboard')
-
-                @include('atlantis-theme.partials.sidebar-separator', ['section' => 'Components'])
-
-                @include('atlantis-theme.examples.partials.companies')
-                @include('atlantis-theme.examples.partials.employes')
-
-                @stack('sidebar')
+                @yield('navbar-items')
+                @stack('sidebar-extra')
             </ul>
         </div>
     </div>
